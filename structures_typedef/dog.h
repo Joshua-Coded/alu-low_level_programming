b2a7e6c585a2dc9cd9409c98255dc9529f74d657
joshbam-joshwilly <j.alana@alustudent.com>
2024-10-27 10:50:10 +0200

    dog header file

---
+++ structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/*
+ *dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+/**
+ * struct dog---- struct that stores some information about a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ *  Description: struct called "dog" that stores it's name, age, and owner.
+ */
+
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
