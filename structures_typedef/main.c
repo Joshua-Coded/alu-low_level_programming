e7bcf67e92940d932838034a73025ab40233349d
joshbam-joshwilly <j.alana@alustudent.com>
2024-10-27 10:54:27 +0200

    some code in the main file

---
+++ structures_typedef/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+    struct dog my_dog;
+    my_dog.name = "Bella";
+    my_dog.age = 10.0;
+    my_dog.owner = "Bog";
+
+    printf("My name is %s, and I am %.1f : ) - Wogggg!\n", my_dog.name, my_dog.age, my_dog.owner);
+    return 0;
+}
