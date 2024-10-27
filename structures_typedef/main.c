33ecd8cc99cbaf5d27325a901ce227f85c9eced0
joshbam-joshwilly <j.alana@alustudent.com>
2024-10-27 10:58:38 +0200

    some codes

--- structures_typedef/main.c
+++ structures_typedef/main.c
@@ -7,13 +7,13 @@
  * Return: Always 0.
  */
 
-int main(void)
+int main()
 {
     struct dog my_dog;
     my_dog.name = "Bella";
     my_dog.age = 10.0;
     my_dog.owner = "Bog";
 
-    printf("My name is %s, and I am %.1f : ) - Wogggg!\n", my_dog.name, my_dog.age, my_dog.owner);
+    printf("My name is %s, and I am %.1f : ) - Wogggg!\n", my_dog.name, my_dog.age);
     return 0;
 }
